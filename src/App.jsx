af39b8ac9f269c137d6e9a526d22b11b3718e3e4
girishdigge <girishdigge@gmail.com>
2024-04-12 05:23:09 +0530

    project init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+const App = () => {
+  return (
+    <div>
+      <section>Hero</section>
+      <section>Parralax</section>
+      <section>Services</section>
+      <section>Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section>Contact</section>
+      {/* <section></section> */}
+    </div>
+  );
+};
+
+export default App;
