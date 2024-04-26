e5ad866672b5da49cb38dd19b15cb9dc85ecf205
girishdigge <girishdigge@gmail.com>
2024-04-26 15:40:19 +0530

    syte complete

--- src/Components/section4/Section4.jsx
+++ src/Components/section4/Section4.jsx
@@ -2,9 +2,36 @@ import './section4.scss';
 const Section4 = () => {
   const blogs = [
     {
-      image: '1.jpg',
+      image: '1.png',
       date: '21 June 2024',
-      title: 'Blog1',
+      title: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+      author: 'John Doe',
+      content:
+        ' Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla, repellendus!',
+      link: '#',
+    },
+    {
+      image: '2.png',
+      date: '21 June 2024',
+      title: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+      author: 'John Doe',
+      content:
+        ' Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla, repellendus!',
+      link: '#',
+    },
+    {
+      image: '3.png',
+      date: '21 June 2024',
+      title: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+      author: 'John Doe',
+      content:
+        ' Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla, repellendus!',
+      link: '#',
+    },
+    {
+      image: '1.png',
+      date: '21 June 2024',
+      title: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
       author: 'John Doe',
       content:
         ' Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla, repellendus!',
@@ -19,18 +46,17 @@ const Section4 = () => {
             <img src='./tools.png' alt='tools' className='contain' />
             <img src='./building.png' alt='' className='contain scale-125' />
             <div className='absolute inset-0 flex flex-row  gap-2 '>
-              <div className='w-1/3 '></div>
-              <div className='flex flex-col items-center justify-center'>
+              <div className='w-1/4'></div>
+              <div className='flex flex-col justify-center'>
                 <h1 className='text-2xl'>Confused? How it works?</h1>
                 <h2 className='text-5xl font-bold'>Book a Demo!</h2>
-
                 <div className='inputs1  mt-2'>
                   <label htmlFor='number'>+91</label>
                   <input
                     type='text'
                     id='number'
                     name='number'
-                    placeholder='Enter phone number.'
+                    placeholder='Enter your phone number.'
                   />
                   <button className='hover:scale-105 '>Book Demo</button>
                 </div>
@@ -39,18 +65,33 @@ const Section4 = () => {
           </div>
         </div>
       </div>
-      <div className='flex flex-col m-14'>
+      <div className='flex flex-col m-14 mt-6'>
         <div className='flex items-center gap-6'>
-          <h1 className='text-2xl font-bold'>SYTE Blogs</h1>
+          <h1 className='text-2xl font-bold ml-4'>SYTE Blogs</h1>
           <hr className='w-20 border-2 border-syte-red' />
         </div>
-        {/* <div className='grid grid-cols-4'>
-          <div>
-            {blogs.map(blog,index)=({
-              
-            })}
+        <div className='grid grid-cols-1 p-4 pt-2'>
+          <div className='flex gap-4'>
+            {blogs.map((blog, index) => (
+              <div
+                key={index}
+                className='flex flex-col gap-2 bg-white  rounded-xl hover:scale-105  transition duration-500 ease-in-out'
+              >
+                <img
+                  src={blog.image}
+                  alt='image'
+                  className='contain rounded-t-xl h-64'
+                />
+                <h1 className='font-semibold text-lg ml-2'>{blog.title}</h1>
+                <div className='flex ml-2'>
+                  {blog.author}{' '}
+                  <span className='border border-l-2 border-syte-red ml-2 mr-2'></span>{' '}
+                  {blog.date}
+                </div>
+              </div>
+            ))}
           </div>
-        </div> */}
+        </div>
       </div>
     </div>
   );
