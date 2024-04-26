167bfb9920533c084a5483773b58ebea3d3a6dc6
girishdigge <girishdigge@gmail.com>
2024-04-26 18:50:00 +0530

    scroll bg changes

---
+++ src/lib/sendMail.ts
@@ -0,0 +1,19 @@
+// import { sendMail } from './mail';
+
+// export const Phone = (data: any) => {
+//   const { number } = data;
+//   const htmlContent = `
+//     <div style="display: flex; flex-direction: row;">
+//       <div style="font-weight: bold; color: #1f3563; font-size: 32px; margin-right: 10px;">Phone:</div>
+//       <div style="color: #ffffff; font-size: 32px;">${number}</div>
+//     </div>
+//       </div>
+// `;
+
+//   sendMail({
+//     to: 'girishdigge@gmail.com',
+//     name: 'SYTE', // You can pass a default name or use the name from data
+//     subject: 'Call Request from SYTE website',
+//     body: htmlContent, // Pass the HTML content
+//   });
+// };
